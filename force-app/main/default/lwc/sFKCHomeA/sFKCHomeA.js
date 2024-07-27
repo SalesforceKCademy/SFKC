3749ddb14542169444439415c8360d91d3dcd649
SalesforceKCademy <102029433+kalyanMalyala@users.noreply.github.com>
2024-07-28 01:54:52 +0530

    change to desktop

--- force-app/main/default/lwc/sFKCHomeA/sFKCHomeA.js
+++ force-app/main/default/lwc/sFKCHomeA/sFKCHomeA.js
@@ -5,7 +5,7 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 
 export default class SFKCHomeA extends LightningElement {  
-    //Mobile  
+    //Desktop  
     @track isModalOpen = false;
     @track name = '';
     @track phone = '';
